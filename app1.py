3ac315978d98dbe94d3d0e7efa806d381c217a91
Jay Shah <shahjay166@gmail.com>
2020-02-03 21:52:25 -0500

    Complete Dictionary

--- app1.py
+++ app1.py
@@ -15,7 +15,10 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w,data.keys())) >0 :
         yn = input("Did you mean %s instead? Enter Y if yes , or N if no :" % get_close_matches(w,data.keys())[0])
         if yn == "Y":
@@ -29,4 +32,9 @@ def translate(w):
         return"Please Enter a Valid Word"
 
 word = input("Enter a word: ")
-print(translate(word))
+output= translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
